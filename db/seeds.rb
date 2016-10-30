9edbc70365838c63b2e9a520644673124ff59437
Liam Bennett <liamalexanderbennett@gmail.com>
2016-10-30 03:05:06 +0000

    Seeded Stacey Story.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,34 @@ Story.create(name: "Story One", id: 1)
 Story.create(name: "Branch One", id: 2)
 Story.create(name: "Branch Two", id: 3)
 
-Message.create(content: "The story starts. Continue? 'Y' or 'N'", order: 1, story_id: 1)
-Message.create(content: "Someone goes missing. Continue? 'Y' or 'N'", order: 2, story_id: 1)
+Message.create(content: "[NARRATOR]: You will follow the story of Stacey, a 15 year old girl from east London who is talking to a boy she met online called Kev. You will follow the story from Stacey's perspective. Continue Story? 'Y' or 'N'", order: 1, story_id: 1)
+Message.create(content: "[NARRATOR]: Kev invites Stacey to a party at his but Stacey was reluctant was relutant to go. Continue Story? 'Y' or 'N'", order: 2, story_id: 1)
 
-Message.create(content: "Call the emergency services and ask for help? 'A' for yes 'B' for no", order: 3, story_id: 1, branches_to_one: 2, branches_to_two: 3, branches: true)
+Message.create(content: "[KEV]: Hey baby, fancy coming to a party tonight at mine? xxx. Continue Story? 'Y' or 'N'", order: 3, story_id: 1)
+Message.create(content: "[YOU]: Hey, I'm not sure, my mum might not let me xxx. Continue Story? 'Y' or 'N'", order: 4, story_id: 1)
 
-Message.create(content: "They where found. Continue? 'Y' or 'N'", order: 1, story_id: 2)
-Message.create(content: "The end", order: 2, story_id: 2)
+Message.create(content: "[NARRATOR]: Over the course of the day, Kev increased the pressure on Stacey. Continue Story? 'Y' or 'N'", order: 5, story_id: 1)
+Message.create(content: "[KEV]: Your mum doesnt need to know, lie to her and stay your staying at a friends house, come on! xxx. Continue Story? 'Y' or 'N'", order: 6, story_id: 1)
 
-Message.create(content: "They where NEVER found. Continue? 'Y' or 'N'", order: 1, story_id: 3)
-Message.create(content: "The end", order: 2, story_id: 3)
+Message.create(content: "[ADVISE]: In this situation, Stacey could tell her mum above the pressure being applied by the online stranger, or talk to the NSPCC helpline annoymously. Continue Story? 'Y' or 'N'", order: 7, story_id: 1)
+Message.create(content: "[NARRATOR]: Stacey buckles under the pressure and agrees to go to the party at Kev's house. Continue Story? 'Y' or 'N'", order: 8, story_id: 1)
+
+Message.create(content: "[YOU]: Okay then Kev, I'll come. Where is your house? xxx. Continue Story? 'Y' or 'N'", order: 9, story_id: 1)
+Message.create(content: "[NARRATOR]: Stacey tells her mum that shes staying at her friend, but she goes to meet Kev. A couple of hours later Stacey stops replying to messages. Continue Story? 'Y' or 'N'", order: 10, story_id: 1)
+
+Message.create(content: "[MUM]: Are you coming home tonight or staying out stacey? x. Continue Story? 'Y' or 'N'", order: 11, story_id: 1)
+Message.create(content: "[MUM]: Stacey please can you reply, im getting worried.... Continue Story? 'Y' or 'N'", order: 12, story_id: 1)
+
+Message.create(content: "[NARRATOR]: Stacey discovered Kev is not who he said he was, and is also a lot older, there is no party just the two of them. Continue Story? 'Y' or 'N'", order: 13, story_id: 1)
+Message.create(content: "[MUM]: What is going on, where are you???. Continue Story? 'Y' or 'N'", order: 14, story_id: 1)
+
+Message.create(content: "[NARRATOR]: Kev is making Stacey feel uncomftable, she wants to leave but is too scared too, Stacey also wont reply to her mum because she is worried about the conciquences of her lie. Continue Story? 'Y' or 'N'", order: 15, story_id: 1)
+Message.create(content: "[NARRATOR]: Staceys options could be to call the police if Kevs behaviour became inapropriate, or to call her mother, parents care about your safety and will not be angry if you admit your mistake. 'A' to call for help, 'B' to Continue Story.", order: 16, story_id: 1, branches_to_one: 2, branches_to_two: 3, branches: true)
+
+Message.create(content: "[NARRATOR]: Staceys mum picked her up from Kevs safely. Stacey told her mum everything that had happened, they call the police. Continue Story? 'Y' or 'N'", order: 1, story_id: 2)
+Message.create(content: "[NARRATOR]: If you ever find yourself in a similar situation or find yourself too afraid to call or return home, visit ........", order: 2, story_id: 2)
+Message.create(content: "[NARRATOR]: The end", order: 3, story_id: 2)
+
+Message.create(content: "[NARRATOR]: Several worried texts arrive from staceys mum. Stacey tries to leave but kev wont let her.... Continue Story? 'Y' or 'N'", order: 1, story_id: 3)
+Message.create(content: "[NARRATOR]: They where NEVER found. Continue? 'Y' or 'N'", order: 2, story_id: 3)
+Message.create(content: "[NARRATOR]: The end", order: 3, story_id: 3)
